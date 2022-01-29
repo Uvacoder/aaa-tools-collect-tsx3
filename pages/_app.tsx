a1b7098b822f8899d036a7b67b78d2b9221fea8c
maruware <me@maruware.com>
2022-01-29 09:41:38 +0900

    apply chakra-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ChakraProvider } from "@chakra-ui/react";
+
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
-export default MyApp
+
+export default MyApp;
